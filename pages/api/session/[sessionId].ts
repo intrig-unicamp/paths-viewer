921614b862ef7bb2e7b96e423459257e4c937e48
William Melo <william.quintas@gmail.com>
2022-06-01 17:30:17 -0300

    #13: Creates handler for many coordinates in session POST request

--- pages/api/session/[sessionId].ts
+++ pages/api/session/[sessionId].ts
@@ -17,10 +17,7 @@ const SessionApi = async (req: NextApiRequest, res: NextApiResponse) => {
           message: "Param sessionId was not sent.",
         });
       } else {
-        const { statusCode, message } = await addCoordinateToSession(
-          String(sessionId),
-          body
-        );
+        const { statusCode, message } = await handleRequest(sessionId, body);
         res.status(statusCode).send({ statusCode, message });
       }
     }
@@ -33,21 +30,41 @@ const SessionApi = async (req: NextApiRequest, res: NextApiResponse) => {
 
 export default SessionApi;
 
+const handleRequest = async (
+  sessionId,
+  body
+): Promise<{ statusCode: number; message: string }> => {
+  const { coordinates } = body;
+
+  if (!!coordinates && Array.isArray(coordinates) && coordinates.length > 0) {
+    return await addManyCoordinatesToSession(String(sessionId), coordinates);
+  } else {
+    return await addCoordinateToSession(String(sessionId), body);
+  }
+};
+
+const storeCoordinatesData = async (
+  sessionId: string,
+  coordinatesData: ICoordinatesData
+): Promise<void> => {
+  const db = admin.firestore();
+  const sessionRef = db.doc(`sessions/${sessionId}`);
+
+  await db.runTransaction(async (transaction) => {
+    const coordinatesCollection = sessionRef.collection("coordinates");
+    const coordinatesRef = coordinatesCollection.doc(
+      `${coordinatesData.date}T${coordinatesData.time}`
+    );
+    transaction.create(coordinatesRef, coordinatesData);
+  });
+};
+
 const addCoordinateToSession = async (
   sessionId: string,
   coordinatesData: ICoordinatesData
 ): Promise<{ statusCode: number; message: string }> => {
   try {
-    const db = admin.firestore();
-    const docRef = db.doc(`sessions/${sessionId}`);
-
-    await db.runTransaction(async (transaction) => {
-      const coordinates = docRef.collection("coordinates");
-      const coordinatesRef = coordinates.doc(
-        `${coordinatesData.date}T${coordinatesData.time}`
-      );
-      transaction.create(coordinatesRef, coordinatesData);
-    });
+    await storeCoordinatesData(sessionId, coordinatesData);
 
     return {
       statusCode: 200,
@@ -65,3 +82,29 @@ const addCoordinateToSession = async (
     return { statusCode: 500, message };
   }
 };
+
+async function addManyCoordinatesToSession(
+  sessionId: string,
+  coordinates: ICoordinatesData[]
+): Promise<{ statusCode: number; message: string }> {
+  try {
+    coordinates.forEach(async (coordinatesData) => {
+      await storeCoordinatesData(sessionId, coordinatesData);
+    });
+
+    return {
+      statusCode: 200,
+      message: `${coordinates.length} coordinates succesfully added to session ${sessionId}`,
+    };
+  } catch (err: any) {
+    const FirestoreErrorCodeAlreadyExists = 6;
+    if (err.code === FirestoreErrorCodeAlreadyExists) {
+      const message = "Once one coordinate is allowed for each date and time.";
+      console.error(message, err);
+      return { statusCode: 400, message };
+    }
+    const message = `Error while adding coordinate to session ${sessionId}.`;
+    console.error(message, err);
+    return { statusCode: 500, message };
+  }
+}
