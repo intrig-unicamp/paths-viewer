91550eb0ff3c322674a5b33c6c9754a73dd5cbc0
William Melo <william.quintas@gmail.com>
2022-06-18 21:35:17 -0300

    #23: Displays last data timestamp on page

--- components/MapContainer/MapContainer.tsx
+++ components/MapContainer/MapContainer.tsx
@@ -2,11 +2,22 @@ import { Grid, List, Paper, Typography } from "@mui/material";
 import { Fragment, FunctionComponent } from "react";
 import { useAppSelector } from "../../config/hooks";
 import { selectSession } from "../../features/sessions/slice";
+import { ICoordinatesData } from "../../models/ICoordinatesData";
 import CoordinatesList from "../CoordinatesList/CoordinatesList";
 import Map from "../Map/Map";
 
 const MapContainer: FunctionComponent = () => {
+  const sortOperator = (
+    { date: date1, time: time1 }: ICoordinatesData,
+    { date: date2, time: time2 }: ICoordinatesData
+  ): number => `${date1}T${time1}` - `${date2}T${time2}`;
+
   const session = useAppSelector(selectSession);
+  const { date, time } =
+    session?.entities
+      ?.map(({ coordinates }) => coordinates?.at(-1))
+      ?.sort(sortOperator)
+      ?.at(-1) ?? {};
 
   return (
     <Grid container spacing={1} sx={{ my: 2 }}>
@@ -36,6 +47,11 @@ const MapContainer: FunctionComponent = () => {
                 ))}
               </List>
             </Paper>
+            {date && time && (
+              <Typography component="p" variant="body1" sx={{ m: 1 }}>
+                {`Last data timestamp: ${date}T${time}`}
+              </Typography>
+            )}
           </Fragment>
         )}
       </Grid>
