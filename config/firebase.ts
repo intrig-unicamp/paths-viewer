827ef19c3b95eaab405bb078a838f97efabdc183
Arthur Simas <arthursimas1@gmail.com>
2023-03-20 20:54:03 -0300

    tweaking firebase env vars

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,10 @@
 import * as admin from "firebase-admin";
-import * as firebaseConfig from "../firebaseConfig.json";
+
+const firebaseConfig = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+};
 
 if (!admin.apps.length) {
   try {
